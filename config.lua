df3a72ee81b7d6912d54d7046892f02093464c4f
kosmik8485 <kosmik8485@gmail.com>
2019-10-08 15:02:22 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,10 +6,16 @@ WIFI_STATION.pwd  = '89857824690'
 -- mqtt
 MQTT_IP        = "192.168.88.249"
 MQTT_PORT      = 1883
-MQTT_CLIENT_ID = "ESP8266-"..node.chipid()
+MQTT_CLIENT_ID = "ESP8266-"  .. node.chipid()
 MQTT_REFRESH   = 10000
-MQTT_USER	   = "pi"
+MQTT_USER	     = "pi"
 MQTT_PASS      = "raspberry"
 
 -- gpio
 DATA_PIN = 2
+
+-- deep sleep
+-- 30 sec
+DSLEEP_TIME = 30000000 
+-- 
+DSLEEP_MODE = 4
