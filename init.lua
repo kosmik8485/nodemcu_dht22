d6b6eadf19df50ce88fcf199325b0305ced9f053
kosmik8485 <kosmik8485@gmail.com>
2019-10-08 09:23:02 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,25 +1,66 @@
-require('config')
+dofile('config.lua')
 
+function startup()
+	if file.open("main.lua") == nil then
+		print("main.lua deleted or renamed!")
+	else
+		print("Running...")
+		dofile("main.lua")
+		--file.close("main.lua")
+	end
+end
+
+-- WIFI events
+wifi_connect_event = function(T)
+	print("Connecting to AP("..T.SSID..") established!")
+	print("Waiting for IP address...")
+	if disconnect_ct ~= nil then disconnect_ct = nil end
+end
+
+wifi_got_ip_event = function(T)
+	print("Wifi connection is ready! IP address is:"..T.IP)
+	print("Startup will resume momentarily, you have 3 seconds to abort.")
+	print("Waiting...")
+	tmr.create():alarm(3000, tmr.ALARM_SINGLE, startup)
+end
+
+wifi_disconnect_event = function(T)
+	if T.reason == wifi.eventmon.reason.ASSOC_LEAVE then
+		return
+	end
+	
+	local total_tries = 75
+	print("\nWifi connection to AP("..T.SSID..") has failed!")
+	
+	for key,val in pairs(wifi.eventmon.reason) do
+		if val == T.reason then
+			print("Disconnect reason: "..val.."("..key..")")
+			break
+		end
+	end
+	
+	if disconnect_ct == nil then
+		disconnect_ct = 1
+	else
+		disconnect_ct = disconnect_ct + 1
+	end
+	
+	if disconnect_ct < total_tries then
+		print("Retrying connection...(attempt "..(disconnect_ct+1).." of "..total_tries..")")
+	else
+		wifi.sta.disconnect()
+		print("Aborting connection to AP!")
+		disconnect_ct = nil
+	end
+end
+
+-- Reg wifi event callback
+wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, wifi_connect_event)
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, wifi_got_ip_event)
+wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, wifi_disconnect_event)
 
 -- TODO: либо коннектимся если точка из настроек есть, либо поднимаем свою мини точку
 print("Starting ESP"..node.chipid().."...")
 
 wifi.setmode(wifi.STATION)
 wifi.sta.config(WIFI_STATION)
-
-print('MAC address:', wifi.sta.getmac())
-
-tmr.create():alarm(1000, tmr.ALARM_AUTO, function(cb_timer)
-	if wifi.sta.getip() == nil then
-		print("Waiting for IP address...")
-	else
-		cb_timer:unregister()
-		print("Wifi connection estabilished")
-		print("IP address: " .. wifi.sta.getip())
-		if file.open("main.lua") ~= nil then
-			dofile("main.lua")
-		else
-			print("main.lua doesn't eixst!")
-		end
-	end
-end)
